88789915976d564f2730b43118202c10f9e86d81
markgm26 <markmart@andrew.cmu.edu>
2025-01-31 14:26:34 -0500

    implemented sortedintlist with both inheritance and delegation

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -9,8 +9,48 @@ package edu.cmu.cs.cs214.rec04;
  * @author Nora Shoemaker
  *
  */
+public class DelegationSortedIntList implements IntegerList {
+    private final SortedIntList sortedList;
+    private int totalAdded;
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+    public DelegationSortedIntList() {
+        this.sortedList = new SortedIntList();
+        this.totalAdded = 0;
+    }
 
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return sortedList.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        totalAdded += list.size();
+        return sortedList.addAll(list);
+    }
+
+    @Override
+    public int get(int index) {
+        return sortedList.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return sortedList.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedList.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return sortedList.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -9,8 +9,22 @@ package edu.cmu.cs.cs214.rec04;
  * @author Nora Shoemaker
  *
  */
-
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
-
-}
+ public class InheritanceSortedIntList extends SortedIntList {
+     private int totalAdded = 0;
+ 
+     @Override
+     public boolean add(int num) {
+         totalAdded++;
+         return super.add(num);  // Calls SortedIntList.add(num), which sorts internally
+     }
+ 
+     @Override
+     public boolean addAll(IntegerList list) {
+         return super.addAll(list);  // Calls SortedIntList.addAll(list), which sorts internally
+     }
+ 
+     public int getTotalAdded() {
+         return totalAdded;
+     }
+ }
+ 
